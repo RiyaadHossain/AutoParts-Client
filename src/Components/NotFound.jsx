7b3ff0a4baa2efd5652ca6178a4b5f8cfadfd71a
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-07-01 16:07:26 +0600

    added

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -4,7 +4,7 @@ import NotFoundImg from '../Assets/Images/NotFound.gif'
 const NotFound = () => {
     return (
         <div className='min-h-[65vh]'>
-            <h1 className='text-4xl md:text-5xl font-bold text-center mt-32 md:mt-2'>404</h1>
+            <h1 className='text-4xl md:text-5xl font-bold text-center mt-32 md:mt-2'>404! Noo</h1>
             <img className='mx-auto' src={NotFoundImg} alt="" />
         </div>
     );
